73fed02ab662188eb43aa12011f3d0cd24cc4a22
Jhon Javin Colque Leon <202001796@est.umss.edu>
2023-10-04 21:50:01 +0200

    consultas mejoradas

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,61 +1,65 @@
 const pool = require("../db");
 
 const obtenerListaCanciones = async (req, res) => {
-  try {
-    const listaCanciones = await pool.query("SELECT * FROM cancion")
-    res.json(listaCanciones.rows)
-  }catch (error) {
-    console.log(error.message)
-  }
-
+    try {
+        const listaCanciones = await pool.query(
+        "SELECT c.*, a.nombre AS artista FROM cancion c, usuario a WHERE idartista=idusuario"
+        );
+        res.json(listaCanciones.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 const obtenerCancion = async (req, res) => {
-  try{
-    const {id} = req.params
-    const result = await pool.query("SELECT * FROM cancion WHERE idcancion = $1", [id])
+    try {
+        const { id } = req.params;
+        const result = await pool.query(
+        "SELECT c.*, a.nombre AS artista FROM cancion c, usuario a WHERE idartista=idusuario and idcancion = $1", [id]
+        );
 
-    if(result.rows.length === 0){
+    if (result.rows.length === 0) {
         return res.status(404).json({
-          message: "Canción no encontrada"
-        })
+        message: "Canción no encontrada",
+        });
     }
-    res.json(result.rows[0])
-  }catch (error) {
-    console.log(error.message)
-  }
-
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 const subirCancion = async (req, res) => {
-  const {idartista, titulo, genero, anio} = req.body;
+    try {
+        const { idartista, titulo, genero, anio } = req.body;
 
-  try{
-    const result = await pool.query(
-      "INSERT INTO cancion (idartista, titulo, genero, anio) VALUES($1, $2, $3, $4) RETURNING *",
-      [idartista, titulo, genero, anio]
-    );
-      res.json(result.rows[0]);
-  }catch(error){
-    console.log(error.message)
-    res.json({error: error.message})
-  }
+        const result = await pool.query(
+        "INSERT INTO cancion (idartista, titulo, genero, anio) VALUES($1, $2, $3, $4) RETURNING *",
+        [idartista, titulo, genero, anio]
+        );
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        res.json({ error: error.message });
+    }
 };
 
 const borrarCancion = async (req, res) => {
-  const {id} = req.params
-  const result = await pool.query("DELETE FROM cancion WHERE idcancion = $1 RETURNING *", [id])
+    const { id } = req.params;
+    const result = await pool.query(
+    "DELETE FROM cancion WHERE idcancion = $1 RETURNING *", [id]
+    );
 
-  if(result.rowCount === 0){
-    return res.status(404).json({
-      message: "Canción no encontrada"
-    })
-  }
-  return res.sendStatus(204)
+    if (result.rowCount === 0) {
+        return res.status(404).json({
+        message: "Canción no encontrada",
+        });
+    }
+    return res.sendStatus(204);
 };
 
 const actualizarCancion = (req, res) => {
-  res.send("Actualizar canción");
+    res.send("Actualizar canción");
 };
 
 module.exports = {
@@ -63,5 +67,5 @@ module.exports = {
     obtenerCancion,
     subirCancion,
     borrarCancion,
-    actualizarCancion
+    actualizarCancion,
 };
