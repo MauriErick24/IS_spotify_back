62a2f708c753a2435429ad7ec72edc448a0d46cb
Jhon Javin Colque Leon <202001796@est.umss.edu>
2023-10-04 17:36:35 +0200

    endpoint cancion por id

---
+++ src/controllers/music.controller.js
@@ -0,0 +1,67 @@
+const pool = require("../db");
+
+const obtenerListaCanciones = async (req, res) => {
+  try {
+    const listaCanciones = await pool.query("SELECT * FROM cancion")
+    res.json(listaCanciones.rows)
+  }catch (error) {
+    console.log(error.message)
+  }
+
+};
+
+const obtenerCancion = async (req, res) => {
+  try{
+    const {id} = req.params
+    const result = await pool.query("SELECT * FROM cancion WHERE idcancion = $1", [id])
+
+    if(result.rows.length === 0){
+        return res.status(404).json({
+          message: "Canción no encontrada"
+        })
+    }
+    res.json(result.rows[0])
+  }catch (error) {
+    console.log(error.message)
+  }
+
+};
+
+const subirCancion = async (req, res) => {
+  const {idartista, titulo, genero, anio} = req.body;
+
+  try{
+    const result = await pool.query(
+      "INSERT INTO cancion (idartista, titulo, genero, anio) VALUES($1, $2, $3, $4) RETURNING *",
+      [idartista, titulo, genero, anio]
+    );
+      res.json(result.rows[0]);
+  }catch(error){
+    console.log(error.message)
+    res.json({error: error.message})
+  }
+};
+
+const borrarCancion = async (req, res) => {
+  const {id} = req.params
+  const result = await pool.query("DELETE FROM cancion WHERE idcancion = $1 RETURNING *", [id])
+
+  if(result.rowCount === 0){
+    return res.status(404).json({
+      message: "Canción no encontrada"
+    })
+  }
+  return res.sendStatus(204)
+};
+
+const actualizarCancion = (req, res) => {
+  res.send("Actualizar canción");
+};
+
+module.exports = {
+    obtenerListaCanciones,
+    obtenerCancion,
+    subirCancion,
+    borrarCancion,
+    actualizarCancion
+};
---
+++ src/routes/music.routes.js
@@ -0,0 +1,12 @@
+const {Router} = require("express")
+const {obtenerListaCanciones, obtenerCancion, subirCancion, borrarCancion, actualizarCancion} = require("../controllers/music.controller")
+
+const router = Router()
+
+router.get("/spotify", obtenerListaCanciones)
+router.get("/spotify/:id", obtenerCancion);
+router.post("/spotify", subirCancion);
+router.delete("/spotify/:id", borrarCancion);
+router.put("/spotify/:id", actualizarCancion);
+
+module.exports = router
