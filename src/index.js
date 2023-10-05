e2f652a83813782bd524a6f2fea92adde5936c2f
Jhon Javin Colque Leon <202001796@est.umss.edu>
2023-10-05 02:20:25 +0200

    archivo gitignore agregado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 const musicRoutes = require("./routes/music.routes")
 
 const app = express()
 
+app.use(cors())
 app.use(morgan("dev"))
-app.use(express.json());
+app.use(express.json())
 app.use(musicRoutes)
 
 
