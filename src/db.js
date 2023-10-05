94f8220af85a9df77ea04f7b7d891863ee8733c3
Jhon Javin Colque Leon <202001796@est.umss.edu>
2023-10-05 21:19:41 +0200

    base de datos en render.com

--- src/db.js
+++ src/db.js
@@ -1,11 +1,7 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user: "postgres",
-    password: "hola",
-    host: "localhost",
-    port: 5432,
-    database: "Prueba"
+    connectionString: "postgres://postgres2:S3RQnMqlJQQ0gkFIeuDmGzXcZk1TvUNe@dpg-ckfb6eeafg7c739kur80-a.oregon-postgres.render.com/spotify2023?sslmode=no-verify"
 })
 
 module.exports = pool
