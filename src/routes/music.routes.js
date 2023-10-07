6696049da7d7453b24e5afd84560f7fe09713a4f
Jhon Javin Colque Leon <202001796@est.umss.edu>
2023-10-07 18:19:34 +0200

    endpoint eliminar cancion de playlist

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require("express")
-const {obtenerListaCanciones, obtenerCancion, subirCancion, borrarCancion, actualizarCancion, buscarCanciones, agregarCancionPlaylist} = require("../controllers/music.controller")
+const {obtenerListaCanciones, obtenerCancion, subirCancion, borrarCancion, actualizarCancion, buscarCanciones, agregarCancionPlaylist, eliminarCancionPlaylist} = require("../controllers/music.controller")
 
 const router = Router()
 
 router.get("/spotify/buscar", buscarCanciones);
 router.post("/spotify/agregarCancionPlaylist", agregarCancionPlaylist);
+router.delete("/spotify/eliminarCancionPlaylist", eliminarCancionPlaylist);
 router.get("/spotify", obtenerListaCanciones);
 router.get("/spotify/:id", obtenerCancion);
 router.post("/spotify", subirCancion);
